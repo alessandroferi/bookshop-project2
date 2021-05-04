883325052f2633977fb4a398d093ecc386ca60b1
alessandro <alessandro.feri@stud.unifi.it>
2021-05-04 10:31:54 +0200

    updated BookRepository interface
     - make it extends MongoRepository<Book, BigInteger>

--- bookshop-project2/src/main/java/com/feri/alessandro/attsw/project/repositories/BookRepository.java
+++ bookshop-project2/src/main/java/com/feri/alessandro/attsw/project/repositories/BookRepository.java
@@ -1,31 +1,11 @@
 package com.feri.alessandro.attsw.project.repositories;
 
-import java.util.List;
-import java.util.Optional;
+import java.math.BigInteger;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.feri.alessandro.attsw.project.model.Book;
 
-public interface BookRepository extends JpaRepository<Book, Long> {
-
-	Optional<Book> findByTitle(String title);
-	
-	@Query("Select b from Book b where b.author = :author")
-	List<Book> findBooksByAuthor(@Param("author") String author);
-	
-	@Query("Select b from Book b where b.title = :title or b.author = :author")
-	List<Book> findBooksByTitleOrAuthor(@Param("title") String title, @Param("author") String author);
-
-	@Query("Select b from Book b where b.title = :title and b.price = :price")
-	Optional<Book> findBookByTitleAndPrice(@Param("title") String title, @Param("price") double price);
-
-	@Query("Select b from Book b where b.price >= :min and b.price <= :max")
-	List<Book> findAllBooksWhosePriceIsWithinAnInterval(@Param("min") double min, @Param("max") double max);
-	
-	@Query("Select b from Book b where b.author = :author order by b.title")
-	List<Book> findAllBooksByAuthorOrderByTitle(@Param("author") String author);
-	
+public interface BookRepository extends MongoRepository<Book, BigInteger>{
+		
 }
