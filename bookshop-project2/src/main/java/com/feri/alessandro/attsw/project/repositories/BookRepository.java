d7f9bf1ea7da20ed6ad68a382d3c556eb95b6fe6
alessandro <alessandro.feri@stud.unifi.it>
2021-05-04 10:36:29 +0200

    created findById and findByTitle methods in BookRepository

--- bookshop-project2/src/main/java/com/feri/alessandro/attsw/project/repositories/BookRepository.java
+++ bookshop-project2/src/main/java/com/feri/alessandro/attsw/project/repositories/BookRepository.java
@@ -1,11 +1,16 @@
 package com.feri.alessandro.attsw.project.repositories;
 
 import java.math.BigInteger;
+import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.feri.alessandro.attsw.project.model.Book;
 
 public interface BookRepository extends MongoRepository<Book, BigInteger>{
+
+	Optional<Book> findById(BigInteger id);
+
+	Optional<Book> findByTitle(String title);
 		
 }
