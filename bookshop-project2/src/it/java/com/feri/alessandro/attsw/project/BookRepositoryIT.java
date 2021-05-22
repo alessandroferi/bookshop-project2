0595fd702dde826212e7e419b41147bf2d8f5895
alessandro <alessandro.feri@stud.unifi.it>
2021-05-22 17:39:06 +0200

    updated integration tests
    - BookRepositoryIT
    - BookRestControllerIT
    - BookshopWebControllerIT

--- bookshop-project2/src/it/java/com/feri/alessandro/attsw/project/BookRepositoryIT.java
+++ bookshop-project2/src/it/java/com/feri/alessandro/attsw/project/BookRepositoryIT.java
@@ -1,8 +1,10 @@
 package com.feri.alessandro.attsw.project;
 import static org.assertj.core.api.Assertions.assertThat;
+import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 
 import java.math.BigInteger;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -13,6 +15,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.feri.alessandro.attsw.project.model.Book;
 import com.feri.alessandro.attsw.project.repositories.BookRepository;
+
 @RunWith(SpringRunner.class)
 @DataMongoTest
 public class BookRepositoryIT {
@@ -40,17 +43,31 @@ public class BookRepositoryIT {
 	}
 	
 	@Test
-	public void test_findByTitle() {
+	public void test_findByTitle_withOneBookInTheRepository() {
 		Book saved = new Book(null, "title", "author", 10.0);	
 		
 		bookRepository.save(saved);
 		
-		assertEquals(saved, bookRepository.findByTitle(saved.getTitle()).get());
+		assertEquals(saved, bookRepository.findByTitle(saved.getTitle()).get(0));
+	}
+	
+	@Test
+	public void test_findByTitle_withMoreBooksInTheRepository() {		
+		Book book1 = new Book(null, "title", "author1", 10.0);
+		Book book2 = new Book(null, "title", "author2", 15.0);
+		Book book3 = new Book(null, "title2", "author3", 15.0);
+		
+		List<Book> books = asList(book1, book2, book3);
+		
+		bookRepository.saveAll(books);
+		
+		assertThat(bookRepository.findByTitle("title")).containsExactly(book1,book2).doesNotContain(book3);	
+		
 	}
 	
 	@Test
 	public void test_findByTitle_not_found() {
-		assertThat(bookRepository.findByTitle("title")).isNotPresent();
+		assertThat(bookRepository.findByTitle("title")).isEmpty();
 	}
 	
 }
