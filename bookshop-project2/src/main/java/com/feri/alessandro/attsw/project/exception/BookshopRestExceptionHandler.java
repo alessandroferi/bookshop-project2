26ef2882fbf8eaad262863b3fb91ef5302a33ca9
alessandro <alessandro.feri@stud.unifi.it>
2021-07-06 10:55:46 +0200

    removed useless argument from BookNotFound exception Hanlder

--- bookshop-project2/src/main/java/com/feri/alessandro/attsw/project/exception/BookshopRestExceptionHandler.java
+++ bookshop-project2/src/main/java/com/feri/alessandro/attsw/project/exception/BookshopRestExceptionHandler.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class BookshopRestExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(value = BookNotFoundException.class)
-	public ResponseEntity<Object> handleBookNotFoundException(BookNotFoundException exception) {
+	public ResponseEntity<Object> handleBookNotFoundException() {
 		return new ResponseEntity<>("Book not found!", HttpStatus.NOT_FOUND);
 	}
 }
